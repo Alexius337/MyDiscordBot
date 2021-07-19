444e30e0a781d9797696da1a24ba9674b6e0af7a
Alexius <31815385+Alexius337@users.noreply.github.com>
2021-07-19 03:39:42 +0000

    Initial commit

---
+++ jokes.py
@@ -0,0 +1,5 @@
+jokes = [
+  "My wife told me to stop impersonating a flamingo. I had to put my foot down. ",
+  "I went to buy some camo pants but couldn’t find any.",
+  "I failed math so many times at school, I can’t even count."
+]
